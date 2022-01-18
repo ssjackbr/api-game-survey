b45da451a63c1416a3e3cd01374c1dda21c1169b
Jackson Rodrigues <jjr0910@gmail.com>
2022-01-17 21:49:33 -0300

    Created project api-game-survey initial commit

---
+++ api-game-survey/src/main/java/me/wup/gamesurvey/GamesurveyApplication.java
@@ -0,0 +1,13 @@
+package me.wup.gamesurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamesurveyApplication.class, args);
+    }
+
+}
---
+++ api-game-survey/src/test/java/me/wup/gamesurvey/GamesurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wup.gamesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
