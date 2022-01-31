5d4c55c1663fd3e9caba4749893bbb8f4a33f973
Jackson Rodrigues <jjr0910@gmail.com>
2022-01-30 23:33:40 -0300

    Created list of records

--- app-game-survey/src/pages/Records/index.tsx
+++ app-game-survey/src/pages/Records/index.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import './styles.css';
+import { RecordsResponse } from "./types";
+import { formatDate } from './helpers';
+import Pagination from "./Pagination";
+import { Link } from "react-router-dom";
 
 
+const BASE_URL = 'http://localhost:8080'
 
-function Records(){
-    return(
+function Records() {
 
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
+
+    const [activePage, setActivePage] = useState(0);
+
+    const handlePageChange = (index: number) => {
+        setActivePage(index);
+    }
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?page=${activePage}`)
+            .then(response => setRecordsResponse(response.data));
+    }, [activePage]);
+
+
+    return (
         <div className="page-container">
+            <div className="filters-container records-actions">
+                <Link to="charts">
+                    <button className="action-filters"> VER GRÁFICO </button>
+                </Link>
+            </div>
             <table className="records-table" cellPadding={0} cellSpacing={0}>
                 <thead>
                     <tr>
@@ -19,11 +44,23 @@ function Records(){
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
-                    </tr>
+                    {recordsResponse?.content.map(record => (
+                        <tr key = {record.id}>
+                            <td>{formatDate(record.moment)}</td>
+                            <td>{record.name}</td>
+                            <td>{record.age}</td>
+                            <td className="text-secondary">{record.gamePlatform}</td>
+                            <td>{record.genreName}</td>
+                            <td className="text-primary">{record.gameTitle}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
+                <Pagination 
+                activePage={activePage} 
+                goToPage={handlePageChange}
+                totalPages={recordsResponse?.totalPages}
+                />
         </div>
 
     );
